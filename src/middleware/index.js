195ca00c0a186364c9cca9ab7a3f18b4e1a41733
Julian Lam <julian@nodebb.org>
2023-06-19 17:29:22 -0400

    feat: http signatures support, .sign() and .verify() AP helper methods

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -17,6 +17,7 @@ const privileges = require('../privileges');
 const cacheCreate = require('../cache/lru');
 const helpers = require('./helpers');
 const api = require('../api');
+const activitypub = require('../activitypub');
 
 const controllers = {
 	api: require('../controllers/api'),
@@ -316,3 +317,13 @@ middleware.proceedOnActivityPub = (req, res, next) => {
 
 	next();
 };
+
+middleware.validateActivity = helpers.try(async (req, res, next) => {
+	// Checks the validity of the incoming payload against the sender and rejects on failure
+	const verified = await activitypub.verify(req);
+	if (!verified) {
+		return res.sendStatus(400);
+	}
+
+	next();
+});
